2dab470bf4029e41e43bf4e5542cb0a8b00488f7
bhushandesai123 <125678918+bhushandesai123@users.noreply.github.com>
2024-04-03 23:21:17 +0530

    Update jenkins-install.sh

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # install jenkins
-sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+# sudo yum update -y
+# sudo wget -O /etc/yum.repos.d/jenkins.repo \
+#     https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# sudo yum upgrade -y
+# sudo amazon-linux-extras install java-openjdk11 -y
+# sudo yum install jenkins -y
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+# above is for ec2 
+for ub
 
 # then install git
 sudo yum install git -y
