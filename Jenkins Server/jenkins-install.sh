90abb86d1685bd187e74c84ff1d746513daac6e6
bhushandesai123 <125678918+bhushandesai123@users.noreply.github.com>
2024-04-03 23:26:39 +0530

    Update jenkins-install.sh

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -10,18 +10,54 @@
 # sudo yum install jenkins -y
 # sudo systemctl enable jenkins
 # sudo systemctl start jenkins
-# above is for ec2 
-for ub
+
+
 
 # then install git
-sudo yum install git -y
+# sudo yum install git -y
 
-#then install terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+# #then install terraform
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum -y install terraform
 
-#finally install kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+# #finally install kubectl
+# sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+# sudo chmod +x ./kubectl
+# sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+# above is for ec2 
+# for ubuntu  jeava
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre
+java -version
+openjdk version "17.0.8" 2023-07-18
+OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+
+# for ubuntu jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# install terraform on ubuntu
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common -y
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update -y
+sudo apt-get install terraform -y
+
+//kubectl install
+sudo apt-get update
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#git install  
+sudo apt install git -y
