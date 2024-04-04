fee0214504b4594433d73a5bbeb7dbb498df8aec
bhushandesai123 <125678918+bhushandesai123@users.noreply.github.com>
2024-04-04 22:32:08 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIATEVIM6G5YIJGQSUL"
-  secret_key ="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"
+//access_key = "AKIATEVIM6G5YIJGQSUL"
+ // secret_key ="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"
 }
